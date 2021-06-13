bdaee300cddf6074ffc5b515dd465740e5e6659c
Moumen Hallak <moumen95@gmail.com>
2021-06-13 22:07:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
 const [bessie, sheep, dolly, babe, little] = farmAnimals.split(' ');
-const [blackAndWhite, black, pink] = farmAnimals.split(' ');
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [blackAndWhite, black, pink] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
